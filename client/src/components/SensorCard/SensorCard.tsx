e490c3a072565699e251f7ee762ff599c33543dd
Mikita Buinavets <mikita.buinavets@arua.co>
2026-01-11 18:03:28 +0300

    Fix generating

--- client/src/components/SensorCard/SensorCard.tsx
+++ client/src/components/SensorCard/SensorCard.tsx
@@ -28,7 +28,7 @@ export default function SensorCard({
 
                 <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
                     <Typography variant='h6' component='div'>
-                        Humidity: {humidity?.toFixed(1) ?? '-'}%RH`
+                        Humidity: {humidity?.toFixed(1) ?? '-'}%RH
                     </Typography>
                 </Box>
 
